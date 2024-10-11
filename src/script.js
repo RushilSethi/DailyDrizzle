b15fc763bf58e9fa973d53d421e960930f9ae4d2
Rushil <rushilsethi333@gmail.com>
2024-10-11 20:14:38 +0530

    added dynamic weather icons

--- src/script.js
+++ src/script.js
@@ -43,10 +43,25 @@ function displayWeather(city, currentData, forecastData){
     const localDate = document.getElementById("current_date");
     localDate.innerText = formatDate(currentData.location.localtime);
 
+    const currentTemp = document.getElementById("current_tempc");
+    currentTemp.innerText = `${currentData.current.temp_c} °C`;
 
+    const currentCondition = document.getElementById("current_condition");
+    currentCondition.innerText = currentData.current.condition.text;
+
+    const currentConditionParent = document.getElementById("primary_current_data");
+    const currentWeatherDataText = document.getElementById("current_weather_data_text")
+    const conditionCode = currentData.current.condition.code; 
+
+    const weatherIcon = document.createElement("span");
+    weatherIcon.innerHTML = getWeatherSVG(conditionCode);
+    currentConditionParent.insertBefore(weatherIcon, currentWeatherDataText);
     
 }
 
+
+
+//HELPER FUNCTIONS
 
 //Function to format date into usable format
 function formatDate(input) {
@@ -64,11 +79,64 @@ function formatDate(input) {
 }
 
 function getDaySuffix(day) {
-    if (day > 3 && day < 21) return "th"; // Covers 11th to 20th
-    switch (day % 10) {
+    if (day > 3 && day < 21) return "th"; // Covers 11th to 20th, REMEMBER THIS
+    switch (day % 10) { 
         case 1: return "st";
         case 2: return "nd";
         case 3: return "rd";
         default: return "th";
     }
 }
+
+
+//function that helps in displaying the correct image as per the weather condition
+const weatherSVGs = {
+    clear: '<img src="../assets/weather/clear.svg" class="h-40 w-40"/>',
+    clouds: '<img src="../assets/weather/clouds.svg" class="h-40 w-40"/>',
+    drizzle: '<img src="../assets/weather/drizzle.svg" class="h-40 w-40"/>',
+    rain: '<img src="../assets/weather/rain.svg" class="h-40 w-40"/>',
+    thunderstorm: '<img src="../assets/weather/thunderstorm.svg" class="h-40 w-40"/>',
+    snow: '<img src="../assets/weather/snow.svg" class="h-40 w-40"/>',
+    atmosphere: '<img src="../assets/weather/atmosphere.svg" class="h-40 w-50"/>'
+};
+
+function getWeatherSVG(conditionCode) {
+    // Clear
+    if ([1000].includes(conditionCode)) {
+        return weatherSVGs.clear;
+    }
+
+    // Clouds
+    else if ([1003, 1006, 1009].includes(conditionCode)) {
+        return weatherSVGs.clouds;
+    }
+
+    // Drizzle
+    else if ([1063, 1150, 1153, 1168, 1171, 1180].includes(conditionCode)) {
+        return weatherSVGs.drizzle;
+    }
+
+    // Rain
+    else if ([1183, 1186, 1189, 1192, 1195, 1240, 1243, 1246, 1273].includes(conditionCode)) {
+        return weatherSVGs.rain;
+    }
+
+    // Thunderstorm
+    else if ([1087, 1273, 1276].includes(conditionCode)) {
+        return weatherSVGs.thunderstorm;
+    }
+
+    // Snow
+    else if ([1066, 1069, 1072, 1114, 1117, 1210, 1213, 1216, 1219, 1222, 1225, 1255, 1258, 1279, 1282].includes(conditionCode)) {
+        return weatherSVGs.snow;
+    }
+
+    // Atmosphere(mist, fog, etc.)
+    else if ([1030, 1135, 1147].includes(conditionCode)) {
+        return weatherSVGs.atmosphere;
+    }
+
+    // Default SVG for unmapped codes
+    return '<img src="../assets/weather/error-cloud.svg" class="h-36 w-36"></img>';
+}
+
